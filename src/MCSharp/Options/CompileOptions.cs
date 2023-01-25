e7074427922fd525a5834b51375912d0b3948466
Keheck <keheck@web.de>
2023-01-25 14:13:01 +0100

    Added comments to command line options

--- src/MCSharp/Options/CompileOptions.cs
+++ src/MCSharp/Options/CompileOptions.cs
@@ -4,19 +4,15 @@ namespace MCSharp.Options;
 
 [Verb("build", true)]
 class CompileOptions {
-    private string _inputFile = "";
-    [Option('i', "infile", SetName = "build")]
-    public string InputFile { 
-        get { return _inputFile; } 
-        set {
-            if(value == null) { throw new ArgumentException("Input File must be set!"); }
-            _inputFile = value;
-        } 
-    }
+    /* Directory that is used as the root directory of the project. If omitted, the current working directory is used */
+    [Option('i', "input-directory")]
+    public string? InputFile { get; set; }
 
-    [Option('w', "log-warnings", SetName = "build")]
+    /* If set, the compiler will print warnings to the standard output. Set by default */
+    [Option('w', "log-warnings")]
     public bool Warn { get; set; } = true;
 
-    [Option('t', "token", SetName = "build")]
+    /* If set, this will be used as the token for any calls to the GitHub API for dependency resolves. If omitted, an unauthenticated approach is used */
+    [Option('t', "token")]
     public string API_Token { get; set; } = "";
 }
--- src/MCSharp/Options/DependencyOptions.cs
+++ src/MCSharp/Options/DependencyOptions.cs
@@ -5,21 +5,33 @@ namespace MCSharp.Options;
 
 [Verb("dependencies", aliases: new string[]{"dependency"})]
 class DependencyOptions {
+    /* If set, the dependency manager will add one or multiple dependency references of {owner}:{repo}:{version} to the mcsharp.xml file and download the required assets to the dependency cache.
+       {owner} is the username of the repository owner, {repo} is the name of the repository and {version} is either the target commit shorthash or the release tag */
     [Option('a', "add")]
     public IEnumerable<string> DependenciesToAdd { get; set; } = Enumerable.Empty<string>();
 
+    /* If set, the dependency manager will look through the referenced repositories {owner}:{repo} and update them to the latest release, 
+       or latest commit if no release can be found */
     [Option('u', "upgrade")]
     public IEnumerable<string> DependenciesToUpgrade { get; set; } = Enumerable.Empty<string>();
 
+    /* If set, the dependency manager will remove unused dependencies from the cache and download missing dependencies as referenced in the mcsharp.xml file*/
     [Option('f', "fetch")]
     public bool Fetch { get; set; }
 
+    /* If set, it removes the dependency {owner}:{repo} from the dependency cache and it's reference from the mcsharp.xml file */
     [Option('r', "remove")]
     public IEnumerable<string> DependenciesToRemove { get; set; } = Enumerable.Empty<string>();
 
+    /* If set, this token will be used to access the GitHub API. It comes with the privilege of 5000 requests/hour instead of the 60 requests/hour for unauthenticated requests */
     [Option('t', "token")]
     public string AccessToken { get; set; } = "";
 
+    /* If set, this one prints out ratelimit information to the user. Be aware that fetching ratelimit info in itself adds to the ratelimit counter. */
     [Option('i', "info")]
     public bool GetInformation { get; set; } = false;
+
+    /* If set, this points to the location of the mcsharp.xml file. If omitted, the current working directory will be assumed. */
+    [Option("sharp-dir")]
+    public string? CompilerDirectory { get; set; }
 }
