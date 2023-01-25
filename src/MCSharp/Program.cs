31e1bc047fa1f3016c215a5c270730811830119f
Keheck <keheck@web.de>
2023-01-25 14:10:17 +0100

    Started work on Dependency managament

--- src/MCSharp/Program.cs
+++ src/MCSharp/Program.cs
@@ -1,10 +1,12 @@
 ﻿using CommandLine;
 using System.Xml;
+using System.Xml.Schema;
 using MimeTypes;
 using System.Reflection;
 using System.Web;
 using System.Text;
 using Octokit;
+using System.Security.Cryptography;
 
 using MCSharp.Options;
 
@@ -16,28 +18,56 @@ class Program {
     private static readonly object objLock = new object();
 
     static void Main(string[] args) {
-        var result = Parser.Default.ParseArguments<CompileOptions, DependencyOptions>(args)
+        lock(objLock) {
+            Task task = Task.Run(async () => await FetchSchemaDefinition());
+            task.Wait();
+        }
+
+        /*var result = Parser.Default.ParseArguments<CompileOptions, DependencyOptions>(args)
             .MapResult(
                 (CompileOptions options) => CompileProject(options),
                 (DependencyOptions options) => ResolveDependencies(options),
                 (errors) => 1
-            );
+            );*/
+    }
+
+    public static XmlDocument GetCompilerSettingsDocument(string? location) {
+        XmlDocument document = new XmlDocument();
+
+        try { 
+            FileInfo compilerDefinition = new DirectoryInfo(location ?? Directory.GetCurrentDirectory()).EnumerateFiles().Where((info) => info.Name == "mcsharp.xml").Single();
+            document.Load(compilerDefinition.Open(System.IO.FileMode.Open));
+
+            //XmlSchema schema = XmlSchema.Read();
+        }
+        catch(InvalidOperationException) {
+            throw new FileNotFoundException($"No file named mcsharp.xml could be found in {location}. It is a required file for setting up the compiler.");
+        }
+
+        return document;
+    }
+
+    // Find a way to check the up-to-dateness of the xml schema definition without too many API calls
+    private async static Task FetchSchemaDefinition() {
+        GitHubClient client = new GitHubClient(new ProductHeaderValue("MCSharp-Compiler", "1.0"));
+        client.Credentials = new Credentials("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");
+        FileInfo settings = new FileInfo("CompilerSettingsDefinition.xsd");
+
+        if(!settings.Exists) {
+            RepositoryContent content = (await client.Repository.Content.GetAllContents("Keheck", "MCSharp", "CompilerSettingsDefinition.xsd")).Single();
+        }
     }
 
     private static int CompileProject(CompileOptions options) {
-        string inputPath = options.InputFile;
-
-        if(MimeTypeMap.GetMimeType(inputPath.Split('.').Last()) != MimeTypeMap.GetMimeType("xml"))
-            throw new ArgumentException("File must have the XML mime type");
-        
-        XmlDocument document = new XmlDocument();
-        document.Load(new FileStream(inputPath, System.IO.FileMode.Open));
+        XmlDocument document = GetCompilerSettingsDocument(options.InputFile);
 
         return 0;
     }
 
     private static int ResolveDependencies(DependencyOptions options) {
         GitHubClient client = new GitHubClient(new ProductHeaderValue("MCSharp-Compiler", "1.0"));
+
+        XmlDocument document = GetCompilerSettingsDocument(options.CompilerDirectory);
 
         if(options.AccessToken == "") {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
@@ -69,10 +99,18 @@ class Program {
                 }
                 catch(AuthorizationException e) {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("A 401 - Unauthorised error has occured. Did you provide an invalid token?");
                     Console.WriteLine(e.Message);
+                    Console.ResetColor();
                     return 1;
                 }
+            }
+        }
+
+        if(options.Fetch) {
+            XmlNode? dependenciesNode = document["dependencies"];
+
+            if(dependenciesNode == null) {
+                Console.ForegroundColor = ConsoleColor.Yellow;
             }
         }
 
