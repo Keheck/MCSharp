1eb2a7a76ce70f5b1790a506131770f3780482b8
Keheck <keheck@web.de>
2023-02-07 15:47:10 +0100

    Turned dependency fetching into async routine

--- src/MCSharp/DependencyManager.cs
+++ src/MCSharp/DependencyManager.cs
@@ -21,19 +21,37 @@ class DependencyManager {
                 Console.ResetColor();
             }
             else {
+                List<Task> fetchTasks = new List<Task>();
+
                 foreach(XmlNode dependency in dependenciesNode.ChildNodes) 
-                    FetchDependency(dependency);
+                    lock(objLock) { 
+                    Task task = Task.Run(async () => await FetchDependency(dependency));
+                    fetchTasks.Add(task); 
+                }
+
+                while(fetchTasks.Count > 0)
+                    fetchTasks = fetchTasks.Where(t => !t.IsCompleted).ToList();
             }
         }
 
         return 0;
     }
 
-    private static void FetchDependency(XmlNode? dependency) {
+    private static async Task FetchDependency(XmlNode? dependency) {
         if(dependency == null) {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("No dependencies were referenced, skipping dependency...");
             return;
         }
+
+        GitHubClient client = Program.client;
+
+        string author = dependency["author"]!.InnerText;
+        string repoName = dependency["repository"]!.InnerText;
+        string version = dependency["version"] == null ? "LATEST" : dependency["version"]!.InnerText;
+
+        Repository repository = await client.Repository.Get(author, repoName);
+
+        repository.Url
     }
 }
