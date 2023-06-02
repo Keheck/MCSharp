1b24d7544293b96225a178caa583929055a19c3a
Keheck <keheck@web.de>
2023-06-02 10:32:48 +0200

    Refined Exception

--- src/MCSharp/Compiler/CompilerException.cs
+++ src/MCSharp/Compiler/CompilerException.cs
@@ -1,26 +1,25 @@
 namespace MCSharp.Compiler;
 
-public class CompilerException: Exception
-{
+public class CompilerException: Exception {
     public int Line { get; }
     public int Column { get; }
     public string SourceFile { get; }
+    public string Reason { get; }
+    public override string Message { get { return $"Compilation error on line {Line} and column {Column} in {SourceFile}: {Reason}"; } }
 
-    public CompilerException(string reason, string sourceFile, int line, int column): base(reason) {
+    public CompilerException(string reason, string sourceFile, int line, int column) {
+        Reason = reason;
         Line = line;
         Column = column;
         SourceFile = sourceFile;
     }
 
-    public CompilerException(string reason, string sourceFile, string sourceCode, int atCharacter): base(reason) {
+    public CompilerException(string reason, string sourceFile, string sourceCode, int atCharacter) {
+        Reason = reason;
         string fromStartToError = sourceCode.Substring(0, atCharacter);
 
         Line = fromStartToError.Count(c => c == '\n');
         Column = fromStartToError.Length - fromStartToError.LastIndexOf('\n');
         SourceFile = sourceFile;
-    }
-
-    public override string ToString() {
-        return $"Compilation error on line {Line} and column {Column} in {SourceFile}: {Message}";
     }
 }
